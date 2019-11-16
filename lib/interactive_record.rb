1e07a81bcb5f281a2308b25535dd6d5b26ec7915
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-16 22:21:48 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -5,4 +5,10 @@ class InteractiveRecord
   def self.table_name
     self.to_s.downcase.pluralize
   end
+  
+  def self.column_names
+    DB[:conn].results_as_hash = true 
+    sql = "PRAGMA table_info('#{table_name}')"
+    table_info = DB[:conn]
+  end
 end
